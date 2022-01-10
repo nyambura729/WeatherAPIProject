781dc7d00e8d85b95e175e748872bfa3e7ffeeb5
nc18860 <nc18860@essex.ac.uk>
2022-01-10 16:05:22 +0000

    Completed formatting/range/null tests for Wind class

--- Project/src/test/java/DTO/Wind.java
+++ Project/src/test/java/DTO/Wind.java
@@ -2,6 +2,8 @@ package DTO;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.regex.Pattern;
+
 public class Wind{
 
 	@JsonProperty("deg")
@@ -17,4 +19,14 @@ public class Wind{
 	public Double getSpeed(){
 		return speed;
 	}
+
+	public boolean checkSpeedFormat(Double speed) {
+		String matchRegex = "\\d+\\.\\d{2}";
+		boolean result = Pattern.matches(matchRegex, String.valueOf(speed));
+		return result;
+	}
+
+	public boolean checkSpeedRange(Double speed) { return (speed >= 0 && speed <= 135);}
+
+	public boolean checkDirectionRange(Integer degrees) { return (degrees >= 0 && degrees <= 360);}
 }
---
+++ Project/src/test/java/WindTests.java
@@ -0,0 +1,45 @@
+import DTO.WeatherDTO;
+import org.junit.jupiter.api.*;
+import DTO.Wind;
+import Connection.ConnectionManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class WindTests {
+    public static WeatherDTO weatherDTO;
+    public static Wind wind;
+
+    @BeforeAll
+    public static void init() {
+        weatherDTO = Injector.injectWeatherDTO(ConnectionManager.getConnectionCity("London"));
+        wind = weatherDTO.getWind();
+    }
+
+    @Test
+    @DisplayName("Check that speed has 2 decimal places")
+    public void checkSpeedHas2DP() { Assertions.assertTrue(wind.checkSpeedFormat(wind.getSpeed()));}
+
+    @Nested
+    @DisplayName("Checking that speed and direction are in valid ranges")
+    class checkRanges {
+        @Test
+        @DisplayName("Check that speed falls between 0m/s and 135m/s inclusive")
+        public void checkSpeedInValidRange() { Assertions.assertTrue(wind.checkSpeedRange(wind.getSpeed()));}
+
+        @Test
+        @DisplayName("Check that direction falls between 0 degrees and 360 degrees inclusve")
+        public void checkDirectionInValidRange() { Assertions.assertTrue(wind.checkDirectionRange(wind.getDeg()));}
+    }
+
+    @Nested
+    @DisplayName("Check that none of the values are null")
+    class checkNotNull {
+        @Test
+        @DisplayName("Check that speed is not null")
+        public void checkSpeedNotNull() { Assertions.assertTrue(wind.getSpeed() != null);}
+
+        @Test
+        @DisplayName("Check that direction is not null")
+        public void checkDirectionNotNull() { Assertions.assertTrue(wind.getDeg() != null);}
+    }
+}
